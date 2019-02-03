f3a5d9ee8a4610d0c4646f0b2fb9cb1aeea26633
Luiz Soares dos Santos Baglie <luizssb.biz@gmail.com>
2019-02-03 06:53:09 -0200

    Initial Commit

---
+++ eSapiensAlg/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  eSapiensAlg
+//
+//  Created by Luiz SSB on 2/3/19.
+//  Copyright © 2019 Luiz SSB. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
